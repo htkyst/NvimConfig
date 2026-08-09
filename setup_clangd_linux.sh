7df0f56518504662225b4c0948f143ba032bbd4c
rp4htkyst <rubybox@outlook.jp>
2026-08-09 21:32:14 +0900

    add neovim clangd setup for linux

---
+++ setup_clangd_linux.sh
@@ -0,0 +1,7 @@
+sudo install clangd jq
+mkdir -p ~/.local/share/nvim/mason/packages/clangd/mason-schemas
+cd ~/.local/share/nvim/mason/packages/clangd
+curl https://raw.githubusercontent.com/clangd/vscode-clangd/master/package.json \
+    | jq .contributes.configuration > mason-schemas/lsp.json
+echo '{"schema_version":"1.1","primary_source":{"type":"local"},"name":"clangd","links":{"share":{"mason-schemas/lsp/clangd.json":"mason-schemas/lsp.json"}}}' \
+    > mason-receipt.json
